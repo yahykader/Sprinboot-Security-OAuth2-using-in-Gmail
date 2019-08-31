a5075e1847773384bc49ea7a2c9f64dd3175efbb
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-08-31 18:48:18 +0200

    add OAuth2 using in Gmail

---
+++ src/main/java/org/Kader/SpringSecurityOAuth2GmailApplication.java
@@ -0,0 +1,13 @@
+package org.Kader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2GmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuth2GmailApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/Kader/controller/UserController.java
@@ -0,0 +1,18 @@
+package org.Kader.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping
+	public Principal getUser(Principal user) {
+		return user;
+	}
+
+}
